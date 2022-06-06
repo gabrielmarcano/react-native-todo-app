b541fc0c9e1b1833ce2966a35c0174d6a0871371
Gabriel Marcano <gabrielmarcano120@gmail.com>
2022-06-06 12:14:07 -0400

    Button example

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('gabriel')
+  const [person, setPerson] = useState({ name: 'Mario', age: 40 })
+
+  const clickHandler = () =>{
+    setName('foo')
+    setPerson({ name: 'Luigi', age: 45})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Update state' onPress={clickHandler}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +30,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
