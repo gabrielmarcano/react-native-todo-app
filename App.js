77bf96d22784cdb208230a270b7d4fc5a6d760fe
Gabriel Marcano <gabrielmarcano120@gmail.com>
2022-06-06 13:33:37 -0400

    Using lists with FlatList

--- App.js
+++ App.js
@@ -1,31 +1,41 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: 'john', key: '1' },
-    { name: 'james', key: '2' },
-    { name: 'jason', key: '3' },
-    { name: 'jack', key: '4' },
-    { name: 'jean', key: '5' },
-    { name: 'jordan', key: '6' },
-    { name: 'justin', key: '7' },
-    { name: 'jeremy', key: '8' },
-    { name: 'jake', key: '9' },
-    { name: 'joel', key: '10' },
+    { name: 'john', id: '1' },
+    { name: 'james', id: '2' },
+    { name: 'jason', id: '3' },
+    { name: 'jack', id: '4' },
+    { name: 'jean', id: '5' },
+    { name: 'jordan', id: '6' },
+    { name: 'justin', id: '7' },
+    { name: 'jeremy', id: '8' },
+    { name: 'jake', id: '9' },
+    { name: 'joel', id: '10' },
   ])
 
 
   return (
     <View style={styles.container}>
-      <ScrollView>
+
+      <FlatList 
+        horizontal={false}
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}/>
+
+      {/* <ScrollView>
         {people.map(item => (
           <View key={item.key}>
             <Text style={styles.item}>{item.name}</Text>
           </View>
         ))}
-      </ScrollView>
+      </ScrollView> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -46,5 +56,7 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: 'pink',
     fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24
   }
 });
