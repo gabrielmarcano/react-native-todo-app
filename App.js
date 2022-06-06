ebe0c41dd840fc762a28b1175c230b168ed01e2d
Gabriel Marcano <gabrielmarcano120@gmail.com>
2022-06-06 15:54:57 -0400

    Using Alert component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
 import Header from './components/Header';
 import TodoItem from './components/TodoItem';
 import AddTodo from './components/AddTodo';
@@ -19,11 +19,20 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [{text: text, key: Math.random().toString() },
-        ...prevTodos
-      ]
-    })
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [
+        {
+          text: 'Understood',
+          onPress: () => console.log('Alert closed')
+        }
+      ])
+    }
   }
 
   return (
